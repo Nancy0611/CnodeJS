f40dda11cccf73cc1131e7fce6e1b4a95c30d8f5
nancy <15718361748@163.com>
2019-03-07 16:06:17 +0800

    1、完成首页和文章页的结构部分获取API数据

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import ArticleCom from '@/components/ArticleCom'
+import MainSec from '@/components/MainSec'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'RootPath',
+      component: MainSec
+    },
+    {
+      path: '/topic/:id',
+      name: 'ArticleRouter',
+      component: ArticleCom
+    }
+  ]
+})
